f239aa1fa3bdf5bc125335e1d1021c78070dae24
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-06-26 14:00:45 +0300

    Last Commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(wrapper){
+    return function(adjective){
+       return `You are ${wrapper}${adjective}${wrapper}!`
+    }
+}
+
+// To call the function in one line
+console.log(wrapAdjective('*')("a hard worker")); // Outputs: "You are *a hard worker*!"
+
+// To call in two lines
+let wrapWithStar = wrapAdjective('*');
+console.log(wrapWithStar("a hard worker")); // Outputs: "You are *a hard worker*!"
